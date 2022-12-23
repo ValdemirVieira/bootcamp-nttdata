491d32164d2e3969c446b66fc8cba8bbab344caa
ValdemirVieira <brenoviera.s@gmail.com>
2022-12-23 17:22:28 -0300

    Projeto reiniciado

---
+++ desafio-projeto5/angular-blog/src/app/components/head-title/head-title.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-head-title',
+  templateUrl: './head-title.component.html',
+  styleUrls: ['./head-title.component.css']
+})
+export class HeadTitleComponent {
+
+}
