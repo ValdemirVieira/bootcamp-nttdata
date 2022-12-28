e4fe38e21e65bb7f97072b538333aa5b6d2b8f48
ValdemirVieira <brenoviera.s@gmail.com>
2022-12-27 21:10:07 -0300

    Componentes dinamizados

--- desafio-projeto5/angular-blog/src/app/components/head-title/head-title.component.ts
+++ desafio-projeto5/angular-blog/src/app/components/head-title/head-title.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-head-title',
   templateUrl: './head-title.component.html',
-  styleUrls: ['./head-title.component.css']
+  styleUrls: ['./head-title.component.css', './head-title.responsive.component.css']
 })
 export class HeadTitleComponent {
 
