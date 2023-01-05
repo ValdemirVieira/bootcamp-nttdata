45f960109c7b5e6a1e75bafdade8243f6ef38e1a
ValdemirVieira <brenoviera.s@gmail.com>
2023-01-05 17:22:08 -0300

    Content atualizado

--- desafio-projeto5/angular-blog/src/app/data/dataFake.ts
+++ desafio-projeto5/angular-blog/src/app/data/dataFake.ts
@@ -6,9 +6,21 @@ export const dataFake = [
         "photoCover": "https://i0.wp.com/www.crossovernerd.com/wp-content/uploads/2020/11/kenshin.jpg?fit=1366%2C768&ssl=1"
     },
     {
+        "id": "2",
+        "title": "NOVOS ANIMES PARA 2023",
+        "description": "Conheca todos os animes que vão estrear em 2023.",
+        "photoCover": "https://jornalismorio.espm.br/wp-content/uploads/2021/12/confira-agora-os-25-melhores-animes-que-ja-foram-criados-1.jpg"
+    },
+    {
+        "id": "3",
+        "title": "TEORIA: A saga de Boruto foi apenas um sonho de Naruto?",
+        "description": "Teoria fala que a história de Boruto não passa do sonho de Naruto no Mugen Tsukuyomi criado por Madara.",
+        "photoCover": "https://i0.wp.com/techwek.com/wp-content/uploads/2020/12/fotos-de-animes-1.jpg?fit=890%2C464&ssl=1"
+    },
+    {
         "id": "4",
         "title": "ANIMES COM PROTAGONISTAS NEGROS",
-        "description": "Vamos lá ver essa temática importante!",
+        "description": "Vamos falar sobre o racismo velado nos animes!",
         "photoCover": "https://foxdiaryfatec.files.wordpress.com/2020/08/89500-black2bcharacter2b-2banime.jpg?w=1360"
     }
 ]
