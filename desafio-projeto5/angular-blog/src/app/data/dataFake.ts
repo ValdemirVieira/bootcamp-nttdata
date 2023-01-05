ae1166c4300512fd19c949917afe7498748f99c0
ValdemirVieira <brenoviera.s@gmail.com>
2023-01-05 17:07:42 -0300

    Content criado

--- desafio-projeto5/angular-blog/src/app/data/dataFake.ts
+++ desafio-projeto5/angular-blog/src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
     {
-        "id": 1,
-        "photo": "aaa",
-        "title": "Temática importante",
-        "description": "Vamos lá!"
+        "id": "1",
+        "title": "NOVO ANIME DE SAMURAI X",
+        "description": "Remake de um dos maiores animes da história é anunciado para 2023. A volta de Rurounin Kenshin!",
+        "photoCover": "https://i0.wp.com/www.crossovernerd.com/wp-content/uploads/2020/11/kenshin.jpg?fit=1366%2C768&ssl=1"
     },
     {
-        "id": 2,
-        "photo": "aaa",
-        "title": "Temática importante",
-        "description": "Vamos lá!"
+        "id": "4",
+        "title": "ANIMES COM PROTAGONISTAS NEGROS",
+        "description": "Vamos lá ver essa temática importante!",
+        "photoCover": "https://foxdiaryfatec.files.wordpress.com/2020/08/89500-black2bcharacter2b-2banime.jpg?w=1360"
     }
 ]
--- desafio-projeto5/angular-blog/src/app/pages/content/content.component.ts
+++ desafio-projeto5/angular-blog/src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -6,7 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
-  photoCover:string = "https://foxdiaryfatec.files.wordpress.com/2020/08/89500-black2bcharacter2b-2banime.jpg?w=1360"
-  contentTitle:string = "TÍTULO DA NOTÍCIA"
-  contentDescription:string = "Conteúdo da notícia"
+  photoCover:string = ""
+  contentTitle:string = ""
+  contentDescription:string = ""
+  private id:string | null = "0"
+
+  constructor(
+    private route:ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe( value =>
+      this.id = value.get("id")
+    )
+
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id:string | null) {
+    const result = dataFake.filter(article => article.id == id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
+  }
 }
